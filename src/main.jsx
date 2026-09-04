c89132d50fbc86e80a332eb6f108ac1fe375b25c
Sumit Bopate <sumitbopate888@gmail.com>
2026-09-04 10:24:53 +0530

    Fix Vercel Analytics integration for Vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-
     <App />
+    <Analytics />
   </BrowserRouter>,
 )
